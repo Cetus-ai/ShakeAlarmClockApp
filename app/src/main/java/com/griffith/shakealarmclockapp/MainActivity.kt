7d2ae7433aa8ee6f25f362b77b66fd5b393b50a0
Cetus-ai <Roman@Teissier.de>
2025-10-11 19:31:30 +0200

    starting with Scaffold

--- app/src/main/java/com/griffith/shakealarmclockapp/MainActivity.kt
+++ app/src/main/java/com/griffith/shakealarmclockapp/MainActivity.kt
@@ -3,21 +3,51 @@ package com.griffith.shakealarmclockapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.griffith.shakealarmclockapp.ui.theme.ShakeAlarmClockAppTheme
+import androidx.compose.ui.graphics.ClipOp
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Text("Hi")
+            Column (
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(top = 100.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ){
+                Text(
+                    text = "Alarms",
+                    fontSize = 30.sp,
+                    color = Color.White,
+                    fontWeight = FontWeight.Bold
+                )
+
+            }
         }
+    }
+}
+
+@Composable
+fun AddButton(onClick: ()-> Unit){
+    FloatingActionButton(
+        onClick = {onClick() },
+    ) {
+        Icon(Icons.Filled.Add, "Add Alarm")
     }
 }
