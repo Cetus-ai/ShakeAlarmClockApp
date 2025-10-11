9353fe0421f80cc12fc45d2c320bf10a14b243a6
Cetus-ai <Roman@Teissier.de>
2025-10-11 16:44:12 +0200

    yey, after 3 hours my Background is grey now

--- app/src/main/java/com/griffith/shakealarmclockapp/MainActivity.kt
+++ app/src/main/java/com/griffith/shakealarmclockapp/MainActivity.kt
@@ -16,32 +16,8 @@ import com.griffith.shakealarmclockapp.ui.theme.ShakeAlarmClockAppTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            ShakeAlarmClockAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            Text("Hi")
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ShakeAlarmClockAppTheme {
-        Greeting("Android")
     }
 }
