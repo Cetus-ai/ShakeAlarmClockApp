2b6b3dbaaefcaad1c5a2495a55a358a73431bfd1
Cetus-ai <Roman@Teissier.de>
2025-10-12 15:36:11 +0200

    Scaffold   --Ceck Button(+)  --Check

--- app/src/main/java/com/griffith/shakealarmclockapp/MainActivity.kt
+++ app/src/main/java/com/griffith/shakealarmclockapp/MainActivity.kt
@@ -8,9 +8,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.darkColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -22,32 +27,41 @@ import androidx.compose.ui.unit.sp
 
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column (
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(top = 100.dp),
-                horizontalAlignment = Alignment.CenterHorizontally
-            ){
-                Text(
-                    text = "Alarms",
-                    fontSize = 30.sp,
-                    color = Color.White,
-                    fontWeight = FontWeight.Bold
+            MaterialTheme(
+                colorScheme = darkColorScheme(
+                    background = Color(0xFF2C2C2E)
                 )
-
+            ){
+                Scaffold(
+                    floatingActionButton = {
+                        FloatingActionButton(onClick = {CreateAlarm()}) {
+                            Icon(Icons.Filled.Add, "Add Alarm")
+                        }
+                    }
+                ){ paddingValues ->
+                    Column (
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(paddingValues),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ){
+                    Text(
+                        text = "Alarms",
+                        fontSize = 30.sp,
+                        color = Color.White,
+                        fontWeight = FontWeight.Bold
+                    )
+                    }
+                }
             }
         }
     }
 }
 
-@Composable
-fun AddButton(onClick: ()-> Unit){
-    FloatingActionButton(
-        onClick = {onClick() },
-    ) {
-        Icon(Icons.Filled.Add, "Add Alarm")
-    }
+fun CreateAlarm(){
+
 }
