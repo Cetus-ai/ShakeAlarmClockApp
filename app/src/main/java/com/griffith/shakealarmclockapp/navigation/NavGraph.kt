83de5393ce65cd9757c9b0f42a977de44d691b5b
Cetus-ai <Roman@Teissier.de>
2025-10-22 17:07:48 +0200

    I tryed different navigations without success. I believ now that my Problem is not my NavGraph. It's my HomeScreen. I want to trigger something in this class which the NavGraph needs to navigate. So i will have to return something instead a better navigation

--- app/src/main/java/com/griffith/shakealarmclockapp/navigation/NavGraph.kt
+++ app/src/main/java/com/griffith/shakealarmclockapp/navigation/NavGraph.kt
@@ -1,20 +1,30 @@
 package com.griffith.shakealarmclockapp.navigation
 
 import androidx.compose.runtime.Composable
-import androidx.navigation.NavHost
-import androidx.navigation.compose.rememberNavController
-import com.griffith.shakealarmclockapp.ui.theme.create.Test
+import androidx.navigation.compose.NavHost
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.composable
 import com.griffith.shakealarmclockapp.ui.theme.home.HomeScreen
+import com.griffith.shakealarmclockapp.ui.theme.create.Test
 
 @Composable
-fun NavGraph(){
-    val navController = rememberNavController()
+fun NavGraph(
+    navController: NavHostController
+){
+    //val navController = rememberNavController()
 
     NavHost(navController = navController, startDestination = "home"){
 
-        composable("home"){
+//        composable<home>{                 //Safe-Navigation
+//            HomeScreen(
+//                addAlarmClick = {
+//                    navController.navigate(route = "create")
+//                }
+//            )
+//        }
+        composable("home"){          //string-Navigation
             HomeScreen(                     //<--- if Button click, navigator guides to "create"
-                addAlarmClick = { navController.navigate(route = "create")}
+                onAddAlarmClick = { navController.navigate(route = "create")}
             )
         }
 
