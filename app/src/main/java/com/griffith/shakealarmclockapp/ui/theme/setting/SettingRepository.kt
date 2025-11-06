576fa2ef801bd0ae363ad5e65c58000e3e02ee44
Cetus-ai <Roman@Teissier.de>
2025-11-06 04:24:43 +0100

    created SettingsRepository for the Settings. Like a property it use set/get. Set to 'Set' the new Date. 'Get' to get the new data if needed.
    The User want a duration of 3min, he set 3min and if the alarm ring and want to know how long he need to snooze he will get the 3min.

---
+++ app/src/main/java/com/griffith/shakealarmclockapp/ui/theme/setting/SettingRepository.kt
@@ -0,0 +1,10 @@
+package com.griffith.shakealarmclockapp.ui.theme.setting
+
+
+class SettingsRepository {
+
+    fun setSnoozeDuration(minutes: Int) {}
+    fun getSnoozeDuration(): Int {}
+    fun setAlarmVolume(volume: Float) {}
+    fun getAlarmVolume(): Float {}
+}
