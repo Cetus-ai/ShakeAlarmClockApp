3d1c550380cfd299c15515cea2a4ef4c88367d61
Cetus-ai <Roman@Teissier.de>
2025-11-07 13:58:57 +0000

    SettingsItem.kt updated and implemented in SettingScreen.kt

--- app/src/main/java/com/griffith/shakealarmclockapp/ui/theme/setting/SettingScreen.kt
+++ app/src/main/java/com/griffith/shakealarmclockapp/ui/theme/setting/SettingScreen.kt
@@ -11,10 +11,15 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -25,10 +30,13 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Preview
 @Composable
 fun SettingScreen(){
     val context = LocalContext.current
+    var snoozeTime by remember { mutableStateOf("10") }
+    var alarmVolume by remember { mutableStateOf(50f) }
 
     Scaffold{ paddingValues ->
         Column(
@@ -66,9 +74,14 @@ fun SettingScreen(){
                     Spacer(modifier = Modifier.height(8.dp))
                 }
                 item {
-                    SettingsItem(
-                        title = "Default Snooze Time",
-                        subtitle = "Input Box for a int/min",
+//                    SettingsItem(
+//                        title = "Default Snooze Time",
+//                        subtitle = "Input Box for a int/min",
+//                    )
+                    TitelInputbox(
+                        titel = "Snooze Time",
+                        value = snoozeTime,
+                        onValueChange = { snoozeTime = it }
                     )
                 }
 
@@ -77,9 +90,10 @@ fun SettingScreen(){
                 }
 
                 item {
-                    SettingsItem(
-                        title = "Alarm Volume",
-                        subtitle = "a scalebar",
+                    TitelSlidebar(
+                        titel = "Alarm Volume",
+                        value = alarmVolume,
+                        onValueChange = {alarmVolume = it}
                     )
                 }
 
@@ -98,7 +112,7 @@ fun SettingScreen(){
                         modifier = Modifier
                             .fillMaxWidth()
                     ) {
-                        SettingsItem(
+                        TitelSubtitle(
                             title = "Visit GitHub",
                             subtitle = "Help me improve this app",
                         )
--- app/src/main/java/com/griffith/shakealarmclockapp/ui/theme/setting/SettingsItem.kt
+++ app/src/main/java/com/griffith/shakealarmclockapp/ui/theme/setting/SettingsItem.kt
@@ -3,38 +3,24 @@ package com.griffith.shakealarmclockapp.ui.theme.setting
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 
-//@Composable
-//fun SettingsItem(
-//    title: String,
-//    subtitle: String,
-//){
-//    Card (
-//        modifier = Modifier
-//            .fillMaxSize()
-//    ){
-//        Column {
-//            Text(
-//                text = title,
-//                color = Color.White,
-//                fontWeight = FontWeight.Bold
-//            )
-//            Text(
-//                text = subtitle,
-//            )
-//        }
-//    }
-//}
-
 @Composable
 fun TitelInputbox(
     titel: String,
-    value: Int
+    value: String,
+    onValueChange: (String) -> Unit
 ){
     Card(
         modifier = Modifier
@@ -46,7 +32,62 @@ fun TitelInputbox(
                 color = Color.White,
                 fontWeight = FontWeight.Bold
             )
+            TextField(
+                value = value,
+                onValueChange = onValueChange,
+                placeholder = {Text("Minutes")}
+            )
         }
 
+    }
+}
+
+@ExperimentalMaterial3Api
+@Composable
+fun TitelSlidebar(
+    titel: String,
+    value: Float,
+    onValueChange: (Float) -> Unit
+){
+    //var sliderPostion by remember { mutableStateOf(0f) }
+    Card(
+        modifier = Modifier
+            .fillMaxSize()
+    ){
+        Column {
+            Text(
+                text = titel,
+                color = Color.White,
+                fontWeight = FontWeight.Bold
+            )
+            Slider(
+                value = value,
+                onValueChange = onValueChange,
+                valueRange = 0f..100f
+            )
+        }
+
+    }
+}
+
+@Composable
+fun TitelSubtitle(
+    title: String,
+    subtitle: String,
+){
+    Card (
+        modifier = Modifier
+            .fillMaxSize()
+    ){
+        Column {
+            Text(
+                text = title,
+                color = Color.White,
+                fontWeight = FontWeight.Bold
+            )
+            Text(
+                text = subtitle,
+            )
+        }
     }
 }
