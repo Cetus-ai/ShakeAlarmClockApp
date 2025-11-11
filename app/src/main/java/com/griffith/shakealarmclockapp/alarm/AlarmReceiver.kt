491c9fbef451a6809ddc61f99d60e0011604525b
Cetus-ai <Roman@Teissier.de>
2025-11-11 18:06:01 +0000

    I need a alarm sound

---
+++ app/src/main/java/com/griffith/shakealarmclockapp/alarm/AlarmReceiver.kt
@@ -0,0 +1,16 @@
+package com.griffith.shakealarmclockapp.alarm
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.media.MediaPlayer
+
+class AlarmReceiver : BroadcastReceiver(){
+
+    var mediaPlayer =
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+        TODO("Not yet implemented")
+    }
+
+}
