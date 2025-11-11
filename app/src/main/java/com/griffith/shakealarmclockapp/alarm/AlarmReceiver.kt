0b8450541cf8be3d4de7005479b19c2000514c48
Cetus-ai <Roman@Teissier.de>
2025-11-11 18:30:10 +0000

    Found a alarm

--- app/src/main/java/com/griffith/shakealarmclockapp/alarm/AlarmReceiver.kt
+++ app/src/main/java/com/griffith/shakealarmclockapp/alarm/AlarmReceiver.kt
@@ -4,13 +4,14 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.media.MediaPlayer
+import com.griffith.shakealarmclockapp.R
 
 class AlarmReceiver : BroadcastReceiver(){
 
-    var mediaPlayer =
-
     override fun onReceive(context: Context?, intent: Intent?) {
-        TODO("Not yet implemented")
+        var mediaPlayer = MediaPlayer.create(context, R.raw.air_raid_siren)
+
+
     }
 
 }
