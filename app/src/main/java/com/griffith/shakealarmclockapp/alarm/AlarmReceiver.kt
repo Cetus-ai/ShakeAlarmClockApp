90e4e305508a07c1f9bd96423c36771d90e703de
Cetus-ai <Roman@Teissier.de>
2025-11-13 13:09:36 +0000

    I need 2 more class. Like in a observable pattern
    The Eventclass = AlarmReceiver.kt
    The Eventhandler = AlarmService.kt
    And because its a alarmapp i will need a new screen = WakeUpScreen.kt

--- app/src/main/java/com/griffith/shakealarmclockapp/alarm/AlarmReceiver.kt
+++ app/src/main/java/com/griffith/shakealarmclockapp/alarm/AlarmReceiver.kt
@@ -4,13 +4,18 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.media.MediaPlayer
+import android.os.PowerManager
+import androidx.compose.ui.platform.LocalContext
 import com.griffith.shakealarmclockapp.R
 
 class AlarmReceiver : BroadcastReceiver(){
 
+//    val context = LocalContext.current
+
+
     override fun onReceive(context: Context?, intent: Intent?) {
         var mediaPlayer = MediaPlayer.create(context, R.raw.air_raid_siren)
-
+        val alarmId = intent?.getStringExtra("ALARM_ID")
 
     }
 
