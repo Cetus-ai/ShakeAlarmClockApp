08ab09bfc04bd2c88d03451fe48d624b636d6d8b
Cetus-ai <Roman@Teissier.de>
2025-11-13 13:40:47 +0000

    basic structure

--- app/src/main/java/com/griffith/shakealarmclockapp/alarm/AlarmReceiver.kt
+++ app/src/main/java/com/griffith/shakealarmclockapp/alarm/AlarmReceiver.kt
@@ -6,6 +6,7 @@ import android.content.Intent
 import android.media.MediaPlayer
 import android.os.PowerManager
 import androidx.compose.ui.platform.LocalContext
+import androidx.core.content.ContextCompat
 import com.griffith.shakealarmclockapp.R
 
 class AlarmReceiver : BroadcastReceiver(){
@@ -14,9 +15,13 @@ class AlarmReceiver : BroadcastReceiver(){
 
 
     override fun onReceive(context: Context?, intent: Intent?) {
-        var mediaPlayer = MediaPlayer.create(context, R.raw.air_raid_siren)
+//        var mediaPlayer = MediaPlayer.create(context, R.raw.air_raid_siren)
         val alarmId = intent?.getStringExtra("ALARM_ID")
 
+        val alarmIntent = Intent(context, AlarmService::class.java)
+        alarmIntent.putExtra("ALARM_ID", alarmId)
+//        alarmIntent.putExtra("ALARM_SOUND", mediaPlayer)
+        ContextCompat.startForegroundService(context, alarmIntent)
     }
 
 }
---
+++ app/src/main/java/com/griffith/shakealarmclockapp/alarm/AlarmService.kt
@@ -0,0 +1,20 @@
+package com.griffith.shakealarmclockapp.alarm
+
+import android.content.Intent
+import android.media.MediaPlayer
+import android.os.IBinder
+
+class AlarmService {
+//        var mediaPlayer = MediaPlayer.create(context, R.raw.air_raid_siren)
+    override fun onBind(intent: Intent): IBinder {
+
+    }
+
+    override fun onStart() {
+
+    }
+
+    override fun onStop() {
+
+    }
+}
