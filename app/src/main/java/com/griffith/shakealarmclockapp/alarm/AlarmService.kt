e216bbf90d997bd2fd47104507aa51ffd5fa51f1
Cetus-ai <Roman@Teissier.de>
2025-11-13 14:24:15 +0000

    is starting to look good

--- app/src/main/java/com/griffith/shakealarmclockapp/alarm/AlarmService.kt
+++ app/src/main/java/com/griffith/shakealarmclockapp/alarm/AlarmService.kt
@@ -1,20 +1,45 @@
 package com.griffith.shakealarmclockapp.alarm
 
+import android.app.Notification
+import android.app.Service
 import android.content.Intent
 import android.media.MediaPlayer
 import android.os.IBinder
+import androidx.core.app.NotificationCompat
+import java.security.Provider
+import com.griffith.shakealarmclockapp.R
 
-class AlarmService {
-//        var mediaPlayer = MediaPlayer.create(context, R.raw.air_raid_siren)
+
+class AlarmService : Service(){
+
+    var mediaPlayer = MediaPlayer.create(this, R.raw.air_raid_siren)
+
     override fun onBind(intent: Intent): IBinder {
 
     }
 
-    override fun onStart() {
+    override fun onStartCommand(intent: Intent) {
+        val alarmId = intent.getStringExtra("ALARM_ID")
+        startForeground(1, buildNotification(alarmId))
 
+        mediaPlayer.start()
     }
+
+    fun stopPlayer(){
+        mediaPlayer.stop()
+        mediaPlayer.release()
+    }
+
 
     override fun onStop() {
 
+    }
+
+    fun buildNotification(alarmId: String?): Notification {
+        return NotificationCompat.Builder(this, "alarm")
+            .setContentTitle("Alarm")
+            .setContentText("Alarm is working")
+            .setSmallIcon(R.drawable.alarm_icon)
+            .build()
     }
 }
