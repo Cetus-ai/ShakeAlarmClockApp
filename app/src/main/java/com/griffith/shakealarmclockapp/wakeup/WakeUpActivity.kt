01474d7b5e1d3a1f3faad5689f9a3902310d3d1e
Cetus-ai <Roman@Teissier.de>
2025-11-17 15:09:46 +0000

    The data is processed by WakeUpManager.kt. Now I am trying to transfer the data to WakeUpScreen.kt using WakeUpActivity.kt and call up the screen.

--- app/src/main/java/com/griffith/shakealarmclockapp/wakeup/WakeUpActivity.kt
+++ app/src/main/java/com/griffith/shakealarmclockapp/wakeup/WakeUpActivity.kt
@@ -1,5 +1,44 @@
 package com.griffith.shakealarmclockapp.wakeup
-//- needed to Navigate to WakeUpScreen
-class WakeUpActivity {
 
+import android.os.Build
+import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.ui.graphics.Color
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.griffith.shakealarmclockapp.viewmodel.AlarmViewModel
+
+
+//- needed to Navigate to WakeUpScreen
+class WakeUpActivity : ComponentActivity() {
+//    - Displays UI (screen)
+//    - Reminder list
+//    - Shake animation
+//    - Dismiss/Snooze buttons
+
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+
+        window.addFlags(
+            WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED or
+            WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON
+        )
+
+        setContent {
+
+            MaterialTheme(
+                colorScheme = darkColorScheme(
+                    background = Color(0xFF2C2C2E)
+                )
+            ) {
+                WakeUpScreen(
+
+                )
+            }
+        }
+    }
 }
--- app/src/main/java/com/griffith/shakealarmclockapp/wakeup/WakeUpScreen.kt
+++ app/src/main/java/com/griffith/shakealarmclockapp/wakeup/WakeUpScreen.kt
@@ -2,9 +2,6 @@ package com.griffith.shakealarmclockapp.wakeup
 
 import androidx.activity.ComponentActivity
 
-class WakeUpActivity : ComponentActivity() {
-//    - Displays UI (screen)
-//    - Reminder list
-//    - Shake animation
-//    - Dismiss/Snooze buttons
+class WakeUpScreen() {
+
 }
