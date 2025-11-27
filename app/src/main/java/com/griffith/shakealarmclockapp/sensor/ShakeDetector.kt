7e772418d2feafd56c3e5099a723d72ca83140c1
Cetus-ai <Roman@Teissier.de>
2025-11-27 00:48:55 +0000

    Using Pythagoras' theorem, I can find out how far the mobile phone has moved from its starting position. a² + b² = c². However, it is important to remember that we are dealing with three-dimensional space. In addition, the force of gravity acting on the z-axis must be subtracted.

--- app/src/main/java/com/griffith/shakealarmclockapp/sensor/ShakeDetector.kt
+++ app/src/main/java/com/griffith/shakealarmclockapp/sensor/ShakeDetector.kt
@@ -1,4 +1,23 @@
 package com.griffith.shakealarmclockapp.sensor
 
-class ShakeDetector {
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorListener
+import android.hardware.SensorManager
+import kotlin.math.sqrt
+
+class ShakeDetector : SensorEventListener {
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        val x = event?.values[0]
+        val y = event?.values[1]
+        val z = event?.values[2]
+
+        val acceleration = sqrt(x * x + y * y + z * z) - SensorManager.GRAVITY_EARTH
+
+    }
 }
