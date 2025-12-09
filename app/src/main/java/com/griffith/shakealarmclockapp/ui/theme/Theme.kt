666c8e4a3a302b91fe08cda8e81e26f3ea98cba3
Cetus-ai <Roman@Teissier.de>
2025-12-09 18:53:58 +0000

    Color.kt and Theme.kt reworked

--- app/src/main/java/com/griffith/shakealarmclockapp/ui/theme/Theme.kt
+++ app/src/main/java/com/griffith/shakealarmclockapp/ui/theme/Theme.kt
@@ -8,42 +8,50 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
-//To support dark/light mode
 
-//private val DarkColorScheme = darkColorScheme(
-//    primary = Purple80,
-//    secondary = PurpleGrey80,
-//    tertiary = Pink80
-//)
-//
-//private val LightColorScheme = lightColorScheme(
-//    primary = Purple40,
-//    secondary = PurpleGrey40,
-//    tertiary = Pink40
-//)
-//
-//@Composable
-//fun ShakeAlarmClockAppTheme(
-//    darkTheme: Boolean = isSystemInDarkTheme(),
-//    // Dynamic color is available on Android 12+
-//    dynamicColor: Boolean = true,
-//    content: @Composable () -> Unit
-//) {
-//    val colorScheme = when {
-//        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-//            val context = LocalContext.current
-//            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-//        }
-//
-//        darkTheme -> DarkColorScheme
-//        else -> LightColorScheme
-//    }
-//
-//    MaterialTheme(
-//        colorScheme = colorScheme,
-//        typography = Typography,
-//        content = content
-//    )
-//}
+// Dark Color Scheme
+private val DarkColorScheme = darkColorScheme(
+    primary = AppPurple,
+    secondary = AppBlue,
+    tertiary = AppOrange,
+    background = AppBackgroundDark,
+    surface = AppBackgroundGray,
+    onPrimary = AppTextWhite,
+    onSecondary = AppTextWhite,
+    onBackground = AppTextWhite,
+    onSurface = AppTextWhite
+)
+
+// Light Color Scheme
+private val LightColorScheme = lightColorScheme(
+    primary = AppPurple,
+    secondary = AppBlue,
+    tertiary = AppOrange,
+    background = Color.White,
+    surface = Color.LightGray,
+    onPrimary = AppTextWhite,
+    onSecondary = AppTextWhite,
+    onBackground = AppTextBlack,
+    onSurface = AppTextBlack
+)
+
+@Composable
+fun ShakeAlarmClockAppTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+
+    val colorScheme = if (darkTheme){
+        DarkColorScheme
+    } else{
+        LightColorScheme
+    }
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        content = content
+    )
+}
